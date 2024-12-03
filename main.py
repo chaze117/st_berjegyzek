68f950d930365e4eedd3a68e7eefc6f1908416b0
Chaze <chaze91@gmail.com>
2024-12-03 17:54:19 +0100

    security stuff

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import streamlit as st
 import PyPDF2 
 import re
 from io import BytesIO
-output = None
+import requests
+response = requests.get("https://txwxfzohqokyqsdhbadt.supabase.co/storage/v1/object/sign/berjegyzek/bj.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiZXJqZWd5emVrL2JqLnBkZiIsImlhdCI6MTczMzI0NDMwNCwiZXhwIjoyMDQ4NjA0MzA0fQ.oZcBE5-3y1QjlnTLdzEr5pIEfZXld_0npE-bMJAaLgg")
+response.raise_for_status()
+if "application/pdf" in response.headers.get("Content-Type", ""):
+            pdf_data = BytesIO(response.content)
 def GetPages(ado):
-            object = PyPDF2.PdfReader(r"bj.pdf")
+            object = PyPDF2.PdfReader(pdf_data)
             NumPages = len(object.pages)
             for i in range(0, NumPages):
                 PageObj = object.pages[i]
@@ -29,7 +33,7 @@ ado = st.text_input("Adóazonosító")
 binary_content = None
 if st.button("Bérjegyzék előkészítése") : 
     pn = GetPages(ado)
-    extracted_pdf = extract_pdf_pages("bj.pdf", [pn,pn+1])
+    extracted_pdf = extract_pdf_pages(pdf_data, [pn,pn+1])
 
     st.download_button("Bérjegyzék Letöltése",file_name=ado+'.pdf',data=extracted_pdf)
     
