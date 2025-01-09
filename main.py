a0498eb0fac2aecb8abe0fddd6c02ac0d512bc49
Chaze <chaze91@gmail.com>
2025-01-09 13:33:08 +0100

    Upgrade to selectable pdf

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import PyPDF2
 import re
 from io import BytesIO
 import requests
-response = requests.get("https://txwxfzohqokyqsdhbadt.supabase.co/storage/v1/object/sign/berjegyzek/bj.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiZXJqZWd5emVrL2JqLnBkZiIsImlhdCI6MTczMzI0NDMwNCwiZXhwIjoyMDQ4NjA0MzA0fQ.oZcBE5-3y1QjlnTLdzEr5pIEfZXld_0npE-bMJAaLgg")
-response.raise_for_status()
-if "application/pdf" in response.headers.get("Content-Type", ""):
-            pdf_data = BytesIO(response.content)
-def GetPages(ado):
+from st_supabase_connection import SupabaseConnection
+
+client = st.connection(name="supabase", type=SupabaseConnection)
+def getPDF(link):
+    # response = requests.get("https://txwxfzohqokyqsdhbadt.supabase.co/storage/v1/object/sign/berjegyzek/bj.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJiZXJqZWd5emVrL2JqLnBkZiIsImlhdCI6MTczMzI0NDMwNCwiZXhwIjoyMDQ4NjA0MzA0fQ.oZcBE5-3y1QjlnTLdzEr5pIEfZXld_0npE-bMJAaLgg")
+    response = requests.get(link)
+    response.raise_for_status()
+    if "application/pdf" in response.headers.get("Content-Type", ""):
+                pdf_data = BytesIO(response.content)
+    return pdf_data
+
+def GetPages(ado,pdf_data):
             object = PyPDF2.PdfReader(pdf_data)
             NumPages = len(object.pages)
             for i in range(0, NumPages):
@@ -26,14 +33,20 @@ def extract_pdf_pages(pdf_file, page_numbers):
     output_pdf.seek(0)
     return output_pdf
 
-
+bucketfiles = client.list_objects("Berjegyzekek")
+files = []
+for file in bucketfiles:
+      files.append(file['name'])
 st.title("Bérjegyzék lekérése")
-st.markdown("<h3>Aktuális bérjegyzék: 2024. december</h3>",unsafe_allow_html=True)
+# st.markdown("<h3>Aktuális bérjegyzék: 2024. december</h3>",unsafe_allow_html=True)
+file = st.selectbox("Hónap kiválasztása:",files)
 ado = st.text_input("Adóazonosító")
 binary_content = None
-if st.button("Bérjegyzék előkészítése") : 
-    pn = GetPages(ado)
-    extracted_pdf = extract_pdf_pages(pdf_data, [pn,pn+1])
+if st.button("Bérjegyzék előkészítése") :
+    pdflink = client.get_public_url("Berjegyzekek",file)
+    pdf = getPDF(pdflink)
+    pn = GetPages(ado,pdf)
+    extracted_pdf = extract_pdf_pages(pdf, [pn,pn+1])
 
     st.download_button("Bérjegyzék Letöltése",file_name=ado+'.pdf',data=extracted_pdf)
     
